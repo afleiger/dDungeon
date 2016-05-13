4bd862ac55e50e9f3d84eff3b6270cdde66aa4cd
afleiger <andrew.hfl@gmail.com>
2016-05-13 01:28:39 -0700

    Add files via upload
    new features:
    -Simple test GUI I use for the time being.
    -Refactored how Tiles and Events worked. (I essentially removed Events and combined it into Tile)
    -Created a spawn method which will randomly decide, based on rate numbers, what type of tile will spawn at the location(IE Monster, Chest, Shrine, ETC)
    -Added functionality for printing to the information text box. Currently, anytime a player interacts with a special tile or enters a new room the info box is updated. This is done with a global static screen housed in the Dungeon class.
    -Added a global static Random object, to allow access to a Random Number Generator from anywhere, housed in the Dungeon class.

---
+++ ChrisPro/ChrisistheonProject/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Chrisistheon;
+
+namespace WpfApplication2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Dungeon dung;
+        public MainWindow()
+        {
+            InitializeComponent();
+            dung = new Dungeon();
+            mapBox.Text = dung.GetRoomString();
+            infoBox.Text = dung.infoString;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            dung.moveUp();
+            mapBox.Text = dung.GetRoomString();
+            infoBox.Text = dung.infoString;
+        }
+
+        private void rightButton_Click(object sender, RoutedEventArgs e)
+        {
+            dung.moveRight();
+            mapBox.Text = dung.GetRoomString();
+            infoBox.Text = dung.infoString;
+        }
+
+        private void downButton_Click(object sender, RoutedEventArgs e)
+        {
+            dung.moveDown();
+            mapBox.Text = dung.GetRoomString();
+            infoBox.Text = dung.infoString;
+        }
+
+        private void leftButton_Click(object sender, RoutedEventArgs e)
+        {
+            dung.moveLeft();
+            mapBox.Text = dung.GetRoomString();
+            infoBox.Text = dung.infoString;
+        }
+
+        private void theBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void ExitItem_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void AboutItem_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("P -> Your Party\n# -> Wall", "About");
+        }
+    }
+}
